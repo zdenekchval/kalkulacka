ae12df02aa346c4c9f41062af84f571e99bb03cc
Zdenek Chval <chvalz@jirovcovka.net>
2025-01-05 21:03:25 +0100

    přidány komentáře

--- kalkulacka.js
+++ kalkulacka.js
@@ -1,11 +1,15 @@
+// Tato funkce se spustí automaticky po načtení celé stránky
 window.onload = function() {
+    // Získání čísel které uživatel zadal a vysledku
     var vstup1 = document.getElementById("vstup1");
     var vstup2 = document.getElementById("vstup2");
     var vysledek = document.getElementById("vysledek");
 
+    // Tlačítko pro sčítání
     document.getElementById("tlPlus").onclick = function() {
         var cislo1 = parseFloat(vstup1.value);
         var cislo2 = parseFloat(vstup2.value);
+        // Ověřění jestli uživatel zadal číslo
         if (isNaN(cislo1) || isNaN(cislo2)) {
             vysledek.innerHTML = "Chyba: Zadejte platná čísla!";
         } else {
@@ -13,9 +17,11 @@ window.onload = function() {
         }
     };
 
+    // Tlačítko pro odčítání
     document.getElementById("tlMinus").onclick = function() {
         var cislo1 = parseFloat(vstup1.value);
         var cislo2 = parseFloat(vstup2.value);
+        // Ověřění jestli uživatel zadal číslo
         if (isNaN(cislo1) || isNaN(cislo2)) {
             vysledek.innerHTML = "Chyba: Zadejte platná čísla!";
         } else {
@@ -23,9 +29,11 @@ window.onload = function() {
         }
     };
 
+    // Tlačítko pro násobení
     document.getElementById("tlKrat").onclick = function() {
         var cislo1 = parseFloat(vstup1.value);
         var cislo2 = parseFloat(vstup2.value);
+        // Ověřění jestli uživatel zadal číslo
         if (isNaN(cislo1) || isNaN(cislo2)) {
             vysledek.innerHTML = "Chyba: Zadejte platná čísla!";
         } else {
@@ -33,12 +41,15 @@ window.onload = function() {
         }
     };
 
+    // Tlačítko pro dělení
     document.getElementById("tlDeleno").onclick = function() {
         var cislo1 = parseFloat(vstup1.value);
         var cislo2 = parseFloat(vstup2.value);
         if (isNaN(cislo1) || isNaN(cislo2)) {
             vysledek.innerHTML = "Chyba: Zadejte platná čísla!";
-        } else if (cislo2 === 0) {
+        } 
+        // Ověření jestli se nedělí nulou
+        else if (cislo2 === 0) {
             vysledek.innerHTML = "Chyba: Nelze dělit nulou!";
         } else {
             vysledek.innerHTML = cislo1 / cislo2;
